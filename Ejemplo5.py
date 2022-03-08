a89ccf6690cd81d68cb044ac34c94481a5b4f973
EdithCerdaMonroyHackademy <98177408+EdithCerdaMonroyHackademy@users.noreply.github.com>
2022-03-07 22:25:50 -0600

    First commit

---
+++ Ejemplo5.py
@@ -0,0 +1,39 @@
+#Ejemplo 5
+months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+]
+
+def DiasDelMes(mes):
+  for month in months:
+    if mes not in months:
+      return 0
+    return 1
+      
+def ValidarFecha(mes,dia):
+  respuesta=DiasDelMes(mes)
+  if respuesta==1:
+    if dia>=1 and dia<=30:
+      return True
+    else:
+      return False
+  else:
+    return False    
+    
+        
+      
+
+m= input("Ingresa el Mes:")
+d= int(input("Ingresa el dia:"))
+ValidarFecha(m,d)
+
